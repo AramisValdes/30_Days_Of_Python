d8f16fedc1035196b96b0ab77b48b630ad6c036f
AramisValdes <aramis.valdes99@gmail.com>
2022-05-05 18:18:34 -0400

    init

---
+++ Day 1 Introduction/helloworld.py
@@ -0,0 +1,24 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(3 + 4)  # addition(+)
+print(4 - 3)  # subtraction(-)
+print(3 * 4)  # multiplication(*)
+print(4 % 3)  # modulus(%)
+print(4 / 3)  # division(/)
+print(4 ** 3)  # exponential(**)
+print(4 // 3)  # floor division operator(//)
+
+print('Aramis')
+print('valdes')
+print('\nUnited States')
+print('\nI am enjoying 30 days of python')
+
+# Checking data types
+print(type(10))  # Int
+print(type(3.14))  # Float
+print(type(1 + 3j))  # Complex number
+print(type('Aramis'))  # String
+print(type([1, 2, 3]))  # List
+print(type({'name': 'Aramis'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))  # Set
+print(type((9.8, 3.14, 2.7)))  # Tuple
